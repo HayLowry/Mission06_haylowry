ecd293f0bf14f113bf68e92bf4e726899fc7eca4
haylowry <haylow77@gmail.com>
2023-02-13 22:11:13 -0700

    Add project files.

---
+++ Mission06_haylowry/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission06_haylowry.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_haylowry.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Podcasts()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddMovies()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddMovies(Movie movie)
+        {
+            return View("MovieReceived", movie); 
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission06_haylowry/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_haylowry.Models
+{
+    public class Movie
+    {
+        [Key]
+        [Required]
+        public int MovieID { get; set; }
+        public string cateogry { get; set; }
+        public string title { get; set; }
+        public byte year { get; set; }
+        public string director { get; set; }
+        public string rating { get; set; }
+
+    }
+}
